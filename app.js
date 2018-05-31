a1afd43fda86ee027e78b35999261b80eee5fd0e
Igor <igor.despot@eng.rs>
2018-05-31 11:14:32 +0200

    Fix to entites rended.

--- app.js
+++ app.js
@@ -11,6 +11,8 @@ const flash = require('connect-flash');
 
 const app = express();
 
+app.use(express.static(path.join(__dirname, 'public')));
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
