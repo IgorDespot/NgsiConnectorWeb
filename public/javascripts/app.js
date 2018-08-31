1d054d351128a6e116ba601501306f5e0bfe008a
Igor <igor.despot@eng.rs>
2018-08-31 16:10:15 +0200

    Update app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,4 +1,27 @@
 function replyClick(clicked_id) {
-  console.log(clicked_id)
   window.location.href = `/${clicked_id}`;
 }
+
+$(document).ready(function(){
+  $('#test').attr('disabled',true);
+  $('#rule').keyup(function(){
+    if($(this).val().length !=0)
+        $('#test').attr('disabled', false);            
+    else
+        $('#test').attr('disabled',true);
+})
+
+  $('#main').attr('disabled',true);
+  $('#fiwareService, #fiwareServicePath, #accessToken, #entityid').bind('keyup', function() {
+    if(allFilled()) $('#main').removeAttr('disabled');
+  });
+
+  function allFilled() {
+    var filled = true;
+    $('body input').each(function() {
+        if($(this).val() == '') filled = false;
+    });
+    return filled;
+}
+
+});
