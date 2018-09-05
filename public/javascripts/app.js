5136a686382449f295df741272a996a1061378ea
Igor <igor.despot@eng.rs>
2018-09-05 16:14:26 +0200

    Add form check for create of entities

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -6,17 +6,30 @@ $(document).ready(function(){
   $('#test').attr('disabled',true);
   $('#rule').keyup(function(){
     if($(this).val().length !=0)
-        $('#test').attr('disabled', false);            
+        $('#test').attr('disabled', false);
     else
         $('#test').attr('disabled',true);
 })
 
-  $('#main').attr('disabled',true);
-  $('#fiwareService, #fiwareServicePath, #accessToken, #entityid').bind('keyup', function() {
-    if(allFilled()) $('#main').removeAttr('disabled');
-  });
+$('#main').attr('disabled', true);
+$('#fiwareService, #fiwareService, #accessToken, #entityid').bind('keyup', function () {
+  if(allFilled2()) $('#main').removeAttr('disabled');
+})
 
-  function allFilled() {
+  $('#main2').attr('disabled', true);
+  $('#fiwareServiceUpload, #fiwareServicePathUpload, #accessTokenUpload').bind('keyup', function () {
+    if(allFilled('.test')) $('#main2').removeAttr('disabled');
+  })
+
+  function allFilled(forcheck) {
+    var filled = true;
+    $(forcheck).each(function() {
+        if($(this).val() == '') filled = false;
+    });
+    return filled;
+  }
+
+  function allFilled2() {
     var filled = true;
     $('body input').each(function() {
         if($(this).val() == '') filled = false;
