83aa94df3d4c48e5b6dbacae1284f4cebfa56638
Igor <igor.despot@eng.rs>
2018-11-20 15:24:20 +0100

    Update app

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,30 +11,5 @@ $(document).ready(function(){
         $('#test').attr('disabled',true);
 })
 
-// $('#main').attr('disabled', true);
-// $('#fiwareService, #fiwareService, #accessToken').bind('keyup', function () {
-//   if(allFilled2()) $('#main').removeAttr('disabled');
-// })
-
-//   $('#main2').attr('disabled', true);
-//   $('#fiwareServiceUpload, #fiwareServicePathUpload, #accessTokenUpload').bind('keyup', function () {
-//     if(allFilled('.test')) $('#main2').removeAttr('disabled');
-//   })
-
-//   function allFilled(forcheck) {
-//     var filled = true;
-//     $(forcheck).each(function() {
-//         if($(this).val() == '') filled = false;
-//     });
-//     return filled;
-//   }
-
-//   function allFilled2() {
-//     var filled = true;
-//     $('body input').each(function() {
-//         if($(this).val() == '') filled = false;
-//     });
-//     return filled;
-// }
 
 });
